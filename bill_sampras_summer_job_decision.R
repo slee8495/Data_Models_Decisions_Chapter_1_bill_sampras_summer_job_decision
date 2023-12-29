646cc25a7c22bcb275b6a8fa804d2a0f16dea51e
rsm-sal072 <sal072@ucsd.edu>
2023-12-29 01:08:21 -0800

    12282023

---
+++ bill_sampras_summer_job_decision.R
@@ -0,0 +1,74 @@
+library(shiny)
+library(shinythemes)
+library(shinyWidgets)
+library(DT)
+library(rmarkdown)
+library(shinyjs)
+
+ui <- fluidPage(
+  tags$head(
+    tags$style(HTML("
+     
+      .navbar-brand {
+        font-size: 17px; 
+        margin-bottom: 10px;
+        font-family: 'Verdana', sans-serif; 
+      }
+    "))
+  ),
+  
+  navbarPage(
+    id = "main_nav",
+    title = div(class = "navbar-brand", "Bill Sampras Summer Job Decision"),
+    tabPanel("📊",
+             fluidPage(
+               pickerInput("chapter", "Choose an option:",
+                           choices = c("Case Summary", 
+                                       "Analysis")),
+               uiOutput("chapter")))
+    
+    
+    
+  )
+)
+
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+
+
+
+server <- function(input, output, session) {
+  active_tab <- reactiveVal("chapter")
+  
+  
+  ################################## Chapter 1: Descision Analysis ###########################################
+  
+  
+  output$chapter <- renderUI({
+    req(input$chapter) 
+    
+    file_name <- switch(input$chapter,
+                        "Case Summary" = "summary_bill_sampras_summer_job_decision.html",
+                        "Analysis" = "analysis_bill_sampras_summer_job_decision.html")
+    
+    if (!is.null(file_name)) {
+      tags$iframe(src = file_name, style = "width:100%; height:800px;")
+    }
+  })
+  
+  observeEvent(input$main_nav, {
+    active_tab(input$main_nav)
+  })
+  
+  
+  #########################################################################################################################################################
+  
+  
+}
+
+shinyApp(ui = ui, server = server)
+
+
